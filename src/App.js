851e33b4dbe3ca7d69f650dfd8ab5d964cc803ec
sw <genie_s2@naver.com>
2018-12-24 14:31:23 +0900

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import styled from "styled-components";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Button success />
+                <Button danger />
+            </Container>
+        );
+    }
 }
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100%;
+    background-color: #00838f;
+`;
+const Button = styled.button`
+    border-radius: 50px;
+    padding: 5px;
+    min-width: 120px;
+    color: #fff;
+    font-weight: 600;
+    -webkit-appearance: none;
+    cursor: pointer;
+    &:active,
+    &:focus {
+        outline: none;
+    }
+    background-color: ${props => (props.danger ? "#ec407a" : "#66bb6a")};
+`;
 
 export default App;
